6043d08e3e373b67342701e1b312cd6da0983e8f
Phillip Goldberg <phillip.a.goldberg@vanderbilt.edu>
2018-02-10 13:09:31 -0600

    added react strap

---
+++ we-can-add-stories-to-that/src/components/landing.js
@@ -0,0 +1,14 @@
+'use strict';
+
+import React     from 'react';
+
+/*************************************************************************/
+
+const Landing = () => <div>
+        <div className="row">
+            <h3 className="col-sm-offset-2 col-xs-8">This is my landing page!</h3>
+        </div>
+    </div>;
+
+export default Landing;
+
