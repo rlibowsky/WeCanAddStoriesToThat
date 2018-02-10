6ad3a85cb39ad5ddfc04c74ac462a287d4de1c25
rlibowsky <libowsky@usc.edu>
2018-02-10 15:16:37 -0600

    login page

--- we-can-add-stories-to-that/src/App.js
+++ we-can-add-stories-to-that/src/App.js
@@ -2,9 +2,11 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Redirect } from 'react-router-dom';
-import Header            from './components/header';
-import Landing           from './components/landing';
-import Register          from './components/register';
+import Header from './components/header';
+import Landing from './components/landing';
+import Register from './components/register';
+import Login from './components/login';
+
 class App extends Component {
   render() {
     return <BrowserRouter>
@@ -12,6 +14,7 @@ class App extends Component {
           <Header />
           <Route exact path="/" component={Landing}/>
           <Route path="/register" component={Register} />
+          <Route path="/login" component={Login} />
           </div>
     </BrowserRouter>
     /*
---
+++ we-can-add-stories-to-that/src/components/login.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Container, Col, Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import axios from 'axios';
+import qs from 'qs';
+export default class Login extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+  onSubmit(ev) {
+      ev.preventDefault();
+      const data = {
+          username: document.getElementById('username').value,
+          password: document.getElementById('password').value
+      }
+      /* validate the data */
+      axios.get('/v1/user', qs.stringify(data))
+      .then((res)=>{
+          console.log(res)
+      })
+      
+  }    
+
+  render() {
+    return (
+    <Container>
+      <Form>
+        <FormGroup row>
+          <Label for="username" sm={2}>Username</Label>
+          <Col offset={3} sm={8}>
+            <Input type="text" name="username" id="username" placeholder="Username" />
+          </Col>
+        </FormGroup>
+        <FormGroup row>
+          <Label for="password" sm={2}>Password</Label>
+          <Col offset={3} sm={8}>
+            <Input type="password" name="password" id="password" placeholder="Password" />
+          </Col>
+        </FormGroup>
+        <FormGroup check row>
+          <Col sm={{ size: 10, offset: 2 }}>
+            <Button onClick={this.onSubmit} >Submit</Button>
+          </Col>
+        </FormGroup>
+      </Form>
+    </Container>
+    );
+  }
+}
